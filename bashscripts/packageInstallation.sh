409d26754b9f12a62cedafef3da61a67758fb2ff
mah54 <mrahimi262@gmail.com>
2021-05-04 18:32:13 +0000

    minor fixes

--- bashscripts/packageInstallation.sh
+++ bashscripts/packageInstallation.sh
@@ -39,7 +39,7 @@ apt-get install -y \
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list > /dev/null
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
 apt-get install -y containerd.io
 
